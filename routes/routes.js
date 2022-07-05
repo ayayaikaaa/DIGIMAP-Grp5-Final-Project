fbe6b70d7c913c2b34a1df365298253817dd168e
Angelo Gregorio <g782065@gmail.com>
2022-07-05 20:54:27 +0800

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const app = express();
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public/uploads/')
+      cb(null, __dirname + '/public/uploads/')
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
